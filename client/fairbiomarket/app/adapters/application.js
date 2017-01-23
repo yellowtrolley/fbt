0fe9e3d791444b6ab4a7b6d3dace7b5ac7f0e06c
pablo <yellowtrolley@gmail.com>
2017-01-23 16:25:04 +0100

    set up ember - katharsis - spring - mongodb

--- client/fairbiomarket/app/adapters/application.js
+++ client/fairbiomarket/app/adapters/application.js
@@ -1,4 +1,5 @@
-import FirebaseAdapter from 'emberfire/adapters/firebase';
+import DS from 'ember-data';
 
-export default FirebaseAdapter.extend({
+export default DS.JSONAPIAdapter.extend({
+  namespace: 'api'
 });
