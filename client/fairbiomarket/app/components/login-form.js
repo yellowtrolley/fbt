9026aff16173eb7317075d254805bef5d2e9e06e
pablo <yellowtrolley@gmail.com>
2017-01-25 17:34:55 +0100

    refactor login-form

---
+++ client/fairbiomarket/app/components/login-form.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	actions: {
+    signOut() {
+      this.sendAction('signOut');
+    },
+
+    signIn: function(email, password) {
+      this.sendAction('signIn', email, password);
+    },
+  }
+});
